886a978c2de13a165d1bd9d0fb398eaafaf0c664
andrekrt <andrekrt1922@gmail.com>
2026-03-25 16:24:41 -0300

    update footer email

--- resources/views/vendor/mail/html/subcopy.blade.php
+++ resources/views/vendor/mail/html/subcopy.blade.php
@@ -1,5 +1,13 @@
 <tr>
 <td class="subcopy">
-{!! Illuminate\Mail\Markdown::parse($slot) !!}
+
+<p style="font-size: 14px; line-height: 1.6; color: #6b7280;">
+    Caso não consiga clicar no botão acima, copie e cole o link abaixo no seu navegador:
+</p>
+
+<p style="word-break: break-all; font-size: 13px; color: #111;">
+    {!! Illuminate\Mail\Markdown::parse($slot) !!}
+</p>
+
 </td>
 </tr>
