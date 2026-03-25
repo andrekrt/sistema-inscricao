43fffc5cc6a845854ec03f02242c527313d68389
andrekrt <andrekrt1922@gmail.com>
2026-03-25 16:15:46 -0300

    update header email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,12 @@
 @props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-    <img src="{{ asset('logo.png') }}" alt="{{ config('app.name') }}" style="height: 70px;">
-</a>
-</td>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block; text-align: center;">
+            <img src="https://copa-bacaba.innovakode.com.br/logo.png" alt="{{ config('app.name') }}"
+                style="height: 70px; margin-bottom: 10px;">
+            <div style="font-size: 18px; font-weight: bold; color: #111;">
+                {{ config('app.name') }}
+            </div>
+        </a>
+    </td>
 </tr>
